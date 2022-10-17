a8ff6bed8bdb1dffa661e6e2f7a013bb9affc8f1
puszekjulliuszek <tojabonibykto@gmail.com>
2022-10-17 23:23:43 +0200

    task 2 and tests

---
+++ src/main/java/agh/ics/oop/MapDirections.java
@@ -0,0 +1,50 @@
+package agh.ics.oop;
+
+public enum MapDirections {
+    NORTH,
+    SOUTH,
+    WEST,
+    EAST,
+    NONE;
+
+    @Override
+    public String toString() {
+        return switch (this){
+            case NORTH -> "Północ";
+            case SOUTH -> "Południe";
+            case WEST -> "Zachód";
+            case EAST -> "Wschód";
+            default -> "Brak kieunku";
+        };
+    }
+
+    public MapDirections next(){
+        return switch (this){
+            case NORTH -> EAST;
+            case SOUTH -> WEST;
+            case WEST -> NORTH;
+            case EAST -> SOUTH;
+            default -> NONE;
+        };
+    }
+
+    public MapDirections previous(){
+        return switch (this){
+            case NORTH -> WEST;
+            case SOUTH -> EAST;
+            case WEST -> SOUTH;
+            case EAST -> NORTH;
+            default -> NONE;
+        };
+    }
+    public Vector2d toUnitVector(){
+        return switch (this){
+            case NORTH -> new Vector2d(1,0);
+            case SOUTH -> new Vector2d(-1,0);
+            case WEST -> new Vector2d(0,-1);
+            case EAST -> new Vector2d(0,1);
+            default -> new Vector2d(0,0);
+        };
+    }
+
+}
---
+++ src/main/java/agh/ics/oop/Vector2d.java
@@ -0,0 +1,68 @@
+package agh.ics.oop;
+
+import java.util.Objects;
+
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class Vector2d {
+    private final int x;
+    private final int y;
+
+    public Vector2d(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(%d,%d)".formatted(x, y);
+    }
+
+    public boolean precedes(Vector2d other) {
+        return x <= other.x && y <= other.y;
+    }
+
+    public boolean follows(Vector2d other){
+        return x >= other.x && y >= other.y;
+    }
+    public Vector2d add(Vector2d other) {
+        return new Vector2d(x + other.x, y + other.y);
+    }
+    public Vector2d subtract(Vector2d other){
+        return new Vector2d(x - other.x, y - other.y);
+    }
+
+    public Vector2d upperRight(Vector2d other){
+        return new Vector2d(max(x, other.x), max(y, other.y));
+    }
+
+    public Vector2d lowerLeft(Vector2d other){
+        return new Vector2d(min(x, other.x), min(y, other.y));
+    }
+
+    public Vector2d opposite(){
+        return new Vector2d(-x, -y);
+    }
+
+    @Override
+    public boolean equals(Object other){
+        if(other instanceof Vector2d vector2dOther) {
+            return (x == vector2dOther.x && y == vector2dOther.y);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    public static void main(String[] args) {
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+    }
+}
